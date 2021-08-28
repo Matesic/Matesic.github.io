9ad14cf4a73f20ad8994ea12e6acacc568a7d315
Fran Matešić <Matesic@users.noreply.github.com>
2021-08-28 17:53:29 +0200

    Added theme changing, copyright and removed test ball.

--- sketch.js
+++ sketch.js
@@ -1,46 +1,15 @@
-let unit;
+let theme;
 let font;
-let canvas = {
-    "width": 0,
-    "height": 0,
-    "top": 0
-}
-
-let ball = {
-    "x": 183,
-    "y": 113,
-    "sx": 1,
-    "sy": 1,
-    "r": 50,
-    "s": 10
-}
 
 function preload() {
     font = loadFont('assets/fonts/Inconsolata.ttf');
 }
 
 function setup() {
-    calculateMenuHeight();
-    createCanvas(canvas.width, canvas.height);
-    repositionCanvas(canvas.top);
-
-    unit = windowWidth * 0.05;
-    loadTheme('light');
-
-    ball.x = random(ball.r * 2, canvas.width - ball.r * 2);
-    ball.y = random(ball.r * 2, canvas.height - ball.r * 2);
+    createCanvas(windowWidth, windowHeight);
+    loadThemeFromStorage();
 }
 
 function draw() {
     background(theme.background);
-
-    noStroke();
-    fill(theme.accent);
-    ellipse(ball.x, ball.y, ball.r, ball.r);
-
-    ball.x += ball.sx * ball.s;
-    ball.y += ball.sy * ball.s;
-
-    if (ball.x - ball.r / 2 <= 0 || ball.x + ball.r / 2 >= canvas.width) ball.sx *= -1;
-    if (ball.y - ball.r / 2 <= 0 || ball.y + ball.r / 2 >= canvas.height) ball.sy *= -1;
 }
--- utils.js
+++ utils.js
@@ -1,47 +1,66 @@
-let theme;
+$(document).ready(function() {
+    $('.item').on('click', function() {
+        if (!$(this).hasClass('item-title')) {
+            $('.item').removeClass('item-active');
+            $(this).addClass('item-active');
+        }
+    });
+
+    $('.theme').on('click', function() {
+        if (theme.name == 'light') {
+            loadTheme('dark');
+            $(this).children().css('filter', 'invert(1)');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            loadTheme('light');
+            $(this).children().css('filter', 'invert(0)');
+            localStorage.setItem('theme', 'light');
+        }
+        document.body.classList.toggle('dark-theme');
+    });
+
+    let copyright = document.getElementById('copyright').firstElementChild;
+    copyright.innerHTML = "©Fran Matešić, " + new Date().getFullYear();
+});
 
 window.addEventListener('resize', windowResized);
 
 function windowResized() {
-    let menuHeight = calculateMenuHeight();
-    resizeCanvas(windowWidth, windowHeight - menuHeight);
-    repositionCanvas(menuHeight);
+    resizeCanvas(windowWidth, windowHeight);
 }
 
-function calculateMenuHeight() {
-    let menu = document.getElementById("navbar");
-    let menuStyle = getComputedStyle(menu);
-    canvas.top = menu.offsetHeight + menu.offsetTop;
-    canvas.width = windowWidth;
-    canvas.height = windowHeight - canvas.top;
-}
-
-function repositionCanvas(offset) {
-    let main = document.querySelector('main');
-    main.style.top = offset + 'px';
+function loadThemeFromStorage() {
+    let theme = localStorage.getItem('theme');
+    if (theme == 'dark') {
+        document.getElementById('theme').firstElementChild.style.filter = "invert(1)";
+        document.body.classList.toggle('dark-theme');
+        loadTheme('dark');
+    } else {
+        loadTheme('light');
+    }
 }
 
 function loadTheme(themeName) {
     theme = Utils.themes[themeName];
 }
 let Utils = {
-    "whiteColor": "#F9F7F7",
+    "accent": "#305C91",
     "themes": {
         "light": {
             "name": "light",
-            "background": "#FFF",
-            "font": "#000",
-            "primary": "#DBE2EF",
-            "secondary": "#112D4E",
-            "accent": "#305C91"
+            "background": "#F5F5F5",
+            "font": "#282828",
+            "menu": "#B8C7E0",
+            "menuActive": "#3E5B8E",
+            "menuHover": "#4E72B1"
         },
         "dark": {
             "name": "dark",
-            "background": "#000",
-            "font": "#FFF",
-            "primary": "#112D4E",
-            "secondary": "#DBE2EF",
-            "accent": "#305C91"
+            "background": "#282828",
+            "font": "#F5F5F5",
+            "menu": "#254974",
+            "menuActive": "#6494CE",
+            "menuHover": "#376DAE"
         }
     }
 }
